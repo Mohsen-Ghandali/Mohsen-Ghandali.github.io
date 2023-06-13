09cd656ce8397cb2d3dc7ddab206a783a67e223d
Mohsen2527 <mohsen.2527.gh@gmail.com>
2023-06-13 13:04:12 +0200

    update

---
+++ js/owl-responsive.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1024:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
